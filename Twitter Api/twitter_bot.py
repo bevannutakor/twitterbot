4e53add9e0127c5d93ef0057ca44191d21ff4057
bevannutakor <bevannutakor@icloud.com>
2022-04-06 18:18:07 -0700

    tweet crypto coins

--- Twitter Api/twitter_bot.py
+++ Twitter Api/twitter_bot.py
@@ -1,10 +1,11 @@
 import tweepy
 import os
 import threading
+import time
+import random
+import requests
+from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-#import bs4
-#import requests
-#import re
 
 #ENV setup
 #base directory the file is in
@@ -13,31 +14,54 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 #join base directory with filename
 load_dotenv(os.path.join(BASEDIR, '.env'))
 
-consumer_key = os.getenv('CONSUMER_KEY')
-consumer_secret = os.getenv('CONSUMER_SECRET')
-access_token = os.getenv('ACCESS_TOKEN')
-access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
+def search_price(coin):
+    url = "https://www.google.com/search?q="+coin+"+price"
+    get_html = requests.get(url)
+    parse = BeautifulSoup(get_html.text, 'lxml')
+    text = parse.find("div", attrs={'class':'BNeawe iBp4i AP7Wnd'}).find("div", attrs={'class':'BNeawe iBp4i AP7Wnd'}).text
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-
-auth.set_access_token(access_token, access_token_secret)
-
-api = tweepy.API(auth)
+    print(text)
+    return text
 
 
+#twitter set up
+def create_api():
 
-#Loop works but still throws an error due to duplicate status tweet
-#Todo implement a way to tweet different things on server
-def tweet_stuff():
+    consumer_key = os.getenv('CONSUMER_KEY')
+    consumer_secret = os.getenv('CONSUMER_SECRET')
+    access_token = os.getenv('ACCESS_TOKEN')
+    access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+    auth.set_access_token(access_token, access_token_secret)
+
+    api = tweepy.API(auth)
+
     try:
         api.verify_credentials()
         print("Authentication successful")
+        return api
     except:
         print("error")
 
-    threading.Timer(60.0, tweet_stuff).start()
+#Loop works but still throws an error due to duplicate status tweet
+#Todo implement a way to tweet different things on server
+
+def tweet_price():
+    api = create_api()
+    bitcoin = search_price("bitcoin")
+    litecoin = search_price("litecoin")
+    cardano = search_price("cardano")
+    doge = search_price("doge")
     
-    api.update_status('pain.')
+    tweet = "here are the prices for today: \n" + "bitcoin: " + bitcoin + "\n" + "litecoin: " + litecoin + "\n" + "cardano: " + cardano + "\n" + "doge: " + doge + "\n"
+
+    threading.Timer(86400.0, tweet_price).start() #tweet every 24hrs
+    
+    api.update_status(tweet)
+
     print("message has been tweeted")
 
-tweet_stuff()
+tweet_price()
+
